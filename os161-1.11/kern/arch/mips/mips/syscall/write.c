f9f9b367ea27769338efb7642d4a7fc7c336e668
hmhealey <harrisonmhealey@gmail.com>
2013-03-02 16:59:51 -0500

    A2 Added quick and dirty write which just uses kprintf for everything.

--- os161-1.11/kern/arch/mips/mips/syscall/write.c
+++ os161-1.11/kern/arch/mips/mips/syscall/write.c
@@ -1,8 +1,15 @@
 #include <syscall.h>
+#include <lib.h>
+#include <unistd.h>
 
 int sys_write(int fd, const void *buf, size_t nbytes) {
 
-	// TODO
+	// TODO make this use actual devices/files
+	// TODO don't use kprintf for console printing
+	if (fd != STDOUT_FILENO && fd != STDERR_FILENO) {
+		kprintf("Printing to stdout instead of a file");
+	}
+	kprintf((char *) buf);
 
 	return -1;
 
