e5a0b18b8215f1b016296a1bdbe260541a3dd69f
hmhealey <harrisonmhealey@gmail.com>
2013-03-02 17:57:50 -0500

    A2 Got a half-assed version of exit working\! Shit exists yo\!

--- os161-1.11/kern/arch/mips/mips/syscall/exit.c
+++ os161-1.11/kern/arch/mips/mips/syscall/exit.c
@@ -1,6 +1,14 @@
 #include <syscall.h>
+#include <lib.h>
+#include <thread.h>
+#include <curthread.h>
+#include <addrspace.h>
 
 void sys__exit(int exitcode) {
+
+	kprintf("Exiting...\n");
+
+	thread_exit();
 
 	// TODO
 
