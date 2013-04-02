4645a4f9ff944b69ac66c0d93587d3d3d7fb16c3
hmhealey <harrisonmhealey@gmail.com>
2013-04-02 14:08:54 -0400

    A2 Fixed _exit to close any unclosed file handles on process end.

--- os161-1.11/kern/arch/mips/mips/syscall/exit.c
+++ os161-1.11/kern/arch/mips/mips/syscall/exit.c
@@ -1,10 +1,11 @@
-#include <syscall.h>
-#include <lib.h>
-#include <thread.h>
-#include <curthread.h>
 #include <addrspace.h>
+#include <curthread.h>
+#include <fd.h>
+#include <lib.h>
 #include <process.h>
 #include <synch.h>
+#include <syscall.h>
+#include <thread.h>
 
 void sys__exit(int exitcode) {
 
@@ -12,6 +13,20 @@ void sys__exit(int exitcode) {
 
 	struct process *process = runningprocesses[curthread->t_pid];
 	if (process != NULL) {
+
+		// close any unclosed files
+		lock_acquire(process->p_file_table_lock);
+
+		struct fd **file_table = process->p_file_table;
+		int i;
+		for (i = FIRST_FILE_HANDLE; i < MAX_FILE_HANDLES; i++) {
+			if (file_table[i] != NULL) {
+				_close(file_table, i);
+			}
+		}
+
+		lock_release(process->p_file_table_lock);
+
 		lock_acquire(process->p_exitlock);
 		process->p_finished = 1;
 		process->p_exitcode = exitcode;
